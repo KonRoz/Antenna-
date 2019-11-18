57c09b913d1602706b37ea4dfb0d6f1dd8daa426
Konrad Rozanski <41211324+KonRoz@users.noreply.github.com>
2019-11-18 01:32:21 -0500

    Input and Output Window have been made

--- GUI.py
+++ GUI.py
@@ -1,9 +1,46 @@
 import tkinter as tk
 
+# The input window for the user
+
+
 class InputWindow:
+
+    # the constructor to which root will be passed --> the highest level window here
+    def __init__(self, master):
+
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.calculateButton = tk.Button(self.frame, text='Calculate Optimal Antenna', width=25, command=self.calculate)
+        self.calculateButton.pack()
+        self.frame.pack()
+
+    def calculate(self):
+
+        self.newroot = tk.Tk()
+        self.output = OutputWindow(self.newroot)
+        self.master.destroy()
+
+
+class OutputWindow:
+
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.outputButton = tk.Button(self.frame, text = 'New Window', width = 25,
-                        command = self.new_window)
+        self.outputLabel = tk.Label(self.frame, text='output')
+        self.outputLabel.pack()
+        self.frame.pack()
+
+def main():
+    root = tk.Tk()
+    app = InputWindow(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
 
