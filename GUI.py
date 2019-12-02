b988270f7ae58b0f4c848e311cd5c166e144d3e2
Konrad Rozanski <41211324+KonRoz@users.noreply.github.com>
2019-12-02 00:09:24 -0500

    finishing welcome screen

--- GUI.py
+++ GUI.py
@@ -1,38 +1,72 @@
 import tkinter as tk
+from tkinter.constants import BOTH,YES
 
-# The input window for the user
+FONT_TITLE = ("Mincho", 30, "italic")
+FONT_BUTTONS = ("Bebas Neue", 20, "italic")
+
+image_name = "Earth.gif"
+
+
+class WelcomeWindow:
+
+    def __init__(self, master):
+        self.bg_image = tk.PhotoImage(file=image_name)
+        self.width = self.bg_image.width()
+        self.height = self.bg_image.height()
+
+        self.master = master
+        self.master.title('ANTENNA SELECTOR')
+        self.master.geometry("%dx%d" % (self.width, self.height))
+
+        self.frame = tk.Frame(self.master)
+
+        self.cv = tk.Canvas(self.frame, width=self.width, height=self.height)
+        self.cv.create_image(0, 0, image=self.bg_image, anchor="nw")
+
+        self.welcome_label = tk.Label(self.cv, anchor='center', text="Welcome to the Notre Dame\n Antenna Selector", font=FONT_TITLE)
+        self.input_button = tk.Button(self.cv, anchor='center', text='Select Antenna', width=25, command=self.input_window, font=FONT_BUTTONS)
+        self.quit_button = tk.Button(self.cv, anchor='center', text='Exit', width=25, command=self.quit, font=FONT_BUTTONS)
+
+        self.welcome_label.grid(column=1, row=0, sticky='NS', padx=5, pady=5)
+        self.input_button.grid(column=0, row=1, sticky='NS', padx=10, pady=10)
+        self.quit_button.grid(column=2, row=1, sticky='NS', padx=10, pady=10)
+        self.frame.pack(fill=BOTH, expand=YES)
+
+        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(1, weight=1)
+        self.frame.grid_columnconfigure(2, weight=1)
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.grid_rowconfigure(1, weight=1)
+
+        self.cv.pack(fill=BOTH, expand=YES)
+
+
+    def input_window(self):
+
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = InputWindow(self.newWindow)
+
+    def quit(self):
+
+        self.master.destroy()
 
 
 class InputWindow:
 
-    # the constructor to which root will be passed --> the highest level window here
     def __init__(self, master):
-
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.calculateButton = tk.Button(self.frame, text='Calculate Optimal Antenna', width=25, command=self.calculate)
-        self.calculateButton.pack()
+        self.quitButton = tk.Button(self.frame, text='Quit', width=25, command=self.close_windows)
+        self.quitButton.pack()
         self.frame.pack()
 
-    def calculate(self):
-
-        self.newroot = tk.Tk()
-        self.output = OutputWindow(self.newroot)
+    def close_windows(self):
         self.master.destroy()
 
 
-class OutputWindow:
-
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.outputLabel = tk.Label(self.frame, text='output')
-        self.outputLabel.pack()
-        self.frame.pack()
-
 def main():
     root = tk.Tk()
-    app = InputWindow(root)
+    app = WelcomeWindow(root)
     root.mainloop()
 
 
