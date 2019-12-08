dbe9729810f06947b95eacad85525955d4744e66
mhuebner1 <57882323+mhuebner1@users.noreply.github.com>
2019-12-08 18:09:57 -0500

    Add files via upload
    Added a method to find the diameter for the program

--- Computations.py
+++ Computations.py
@@ -1,6 +1,12 @@
 import math as math
 import tkinter as tk
 
+#This class uses the assumptions that the orbit passes directly above the ground station,
+#is spherical, and the ground station is a fixed station and has a downlink and uplink
+#frequency of 1 GHz
+#The antennas will need to be a high gain directional antenna
+#The antenna will operate at the ideal, which is possible for 1/2 wavelength dipole antennas 100% efficiency
+#Given that the operating frequency is 1 GHz the recommended data rate is 400KB/s
 class Computations:
     height = 0
 
@@ -30,3 +36,14 @@ class Computations:
     #Using previous functions to find the time of contact
     def getTimeOfContact(self):
         return self.s()/self.velocity()
+
+    #This will be the space loss in dB for signal, 0.29979 is the wavelength of a 1 gigahertz signal
+    #The calculation will be 1.76(A decent gain for a directive antennahttp://www.antenna-theory.com/basics/gain.php) = Reciever gain + antenna gain - spaceloss
+    def spaceLoss(self):
+        return ((0.299792458)*(0.299792458))/(4*math.pi()*self.s())
+
+    def receiverGain(self):
+        return ((math.pi()*math.pi())(400000*400000)*.95)/((0.299792458)*(0.299792458))
+
+    def getDiameter(self):
+        return (21*math.sqrt(self.spaceloss()+1.76-self.receiverGain()))^(-1)/1000000000
