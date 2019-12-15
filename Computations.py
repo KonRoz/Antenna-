00ae2476eebca1f0cfcaed8fe0574b71152c025a
mhuebner1 <57882323+mhuebner1@users.noreply.github.com>
2019-12-15 18:44:33 -0500

    Add files via upload

--- Computations.py
+++ Computations.py
@@ -22,8 +22,6 @@ class Computations:
         self.frequencyGHz = self.frequency/(10**9)
         self.powerDb = 10 * math.log(self.power)
         self.earthRadius = 6371000
-
-    #GeoStationary Orbit Gain equations
         self.wavelength = self.speedOfLight / self.frequency
 
         #antennaGain = 27000/beamwidth^2
@@ -31,16 +29,27 @@ class Computations:
         #Ideal equation shannonsLimit = reciverGain + antennaGain + powerDb - spaceloss - rainAttenuation
         #solving for dish dishDiameter
 
-        #Latency Calculation
+    #Author: Matthew Huebner
+    #Email: mhuebner@nd.edu
+    #Latency Calculation
+    #Returns the latency of the signal
+    #Preconditions: none Postconditions: latency-double
     def calcLatency(self):
         latency = self.altitude/(self.speedOfLight)
         return latency
 
+    #Author: Matthew Huebner
+    #Email: mhuebner@nd.edu
+    #Antenna calculated using a halfwavelength dipole
+    #Preconditions: none Postconditions antennaLength-double
     def calcAntennaLength(self):
         antennaLength = self.wavelength / 2
         return antennaLength
 
-
+    #Author: Matthew Huebner
+    #Email: mhuebner@nd.edu
+    #Returns the dish diamter for a stationary orbit
+    #Preconditions none Postconditions geoStationaryDishDiameter-double
     def calcGeoStationaryDishDiameter(self):
         spaceLoss = 147.55 - 20*math.log(self.altitude) - 20*math.log(self.frequencyGHz)
         recieverGain = 10*math.log((math.pi**2*self.dataRate**2*self.efficiency)/self.wavelength)
@@ -49,8 +58,11 @@ class Computations:
         geoStationaryDishDiameter = math.fabs((1/(tempVal/10))*(21/self.frequencyGHz))#removed exponent
         return geoStationaryDishDiameter
 
+    #Author: Matthew Huebner
+    #Email: mhuebner@nd.edu
     #Asynchronous orbit calculations
-    #averaging the values of the altitude across half a pass in order to determine space loss
+    #averaging the values of the altitude across every integer angle of a pass in order to determine space loss
+    #Preconditions: none Postconditions: DishDiameter-double
     def calcDishDiameter(self):
         altitudeSamples = []
         for i in range(84):
@@ -69,7 +81,11 @@ class Computations:
         return DishDiameter
 
 
-        #Solving an angle side side triangle using law of sines
+
+    #Author: Matthew Huebner
+    #Email: mhuebner@nd.edu
+    #Solving an angle side side triangle using law of sines
+    #Precondition theta-int Postcondition:  sample-double
     def altitudeSample(self, theta):
         angle2 = math.degrees(math.asin((math.sin(math.radians(theta+90)))/(self.earthRadius+self.altitude)*self.earthRadius))
         angle3 = 180 - (theta + 90) - angle2
